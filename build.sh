31af1b0431a6e7fde6d8270e83fc9c971e183154
Yeison <yencardonaal@unal.edu.co>
2025-08-30 14:10:55 -0500

    Remove unused `main.py` and update workflow for APK handling
    Deleted the obsolete `main.py` script and adjusted the GitHub Actions workflow for better clarity and functionality. Renamed job, commented out unnecessary directory change, and replaced `cp` with `mv` for APK artifact management.

--- build.sh
+++ build.sh
@@ -16,20 +16,20 @@ if [ -f "$DIST_NAME/manage.py" ]; then
     unzip django*.whl "django/*" -d .
     pip download  --no-deps radiant-compiler
     unzip radiant_compiler*.whl "radiant/*" -d .
-    echo '
-    import sys
-    import os
-    from radiant.compiler import server
-    with open(".p4a", "r", encoding="utf-8") as f:
-      for line in f:
-          if line.strip().startswith("--port"):
-              PORT = int(line.split()[1])
-          if line.strip().startswith("--dist_name"):
-              DIST_NAME = int(line.split()[1])
-    sys.path.append(os.path.join(os.path.dirname(__file__)))
-    sys.path.append(os.path.join(os.path.dirname(__file__), DIST_NAME))
-    server.main(DIST_NAME, ip="localhost", port=PORT)
-    ' > main.py
+    cat > main.py <<- 'EOF'
+import sys
+import os
+from radiant.compiler import server
+with open(".p4a", "r", encoding="utf-8") as f:
+  for line in f:
+      if line.strip().startswith("--port"):
+          PORT = int(line.split()[1])
+      if line.strip().startswith("--dist_name"):
+          DIST_NAME = int(line.split()[1])
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__), DIST_NAME))
+server.main(DIST_NAME, ip="localhost", port=PORT)
+EOF
 fi
 
 rm *.whl
@@ -39,4 +39,4 @@ echo "sqlite3/*" >> whitelist.txt
 echo "lib-dynload/_sqlite3.so" >> whitelist.txt
 echo "*.py" >> whitelist.txt
 
-radiant_p4a apk --arch arm64-v8a
+#radiant_p4a apk --arch arm64-v8a
