460d21b10cbfc71f8e2c8527beb3a6353e11f40a
Yeison <yencardonaal@unal.edu.co>
2025-08-30 12:49:47 -0500

    Remove unused `main.py` and update workflow for APK handling
    Deleted the obsolete `main.py` script and adjusted the GitHub Actions workflow for better clarity and functionality. Renamed job, commented out unnecessary directory change, and replaced `cp` with `mv` for APK artifact management.

--- main.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-from radiant.compiler import server
-
-sys.path.append(os.path.join(os.path.dirname(__file__)))
-sys.path.append(os.path.join(os.path.dirname(__file__), 'project'))
-server.main('project', ip='localhost', port=5000)
