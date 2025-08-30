6d7a3d2fa9a0fea15f6b37632687bb52de561c02
Yeison <yencardonaal@unal.edu.co>
2025-08-30 12:38:09 -0500

    Update radiant_wrapper workflow and adjust setup scripts
    Refactor the GitHub Actions workflow to fix path issues and enhance setup scripts for Django and Radiant Framework handling. Enabled proper extraction of dependencies and adjusted `main.py` to include additional paths for app initialization. Updated `.gitignore` to include Django, Radiant, and additional temporary files.

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import os
 from radiant.compiler import server
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'project'))
 server.main('project', ip='localhost', port=5000)
